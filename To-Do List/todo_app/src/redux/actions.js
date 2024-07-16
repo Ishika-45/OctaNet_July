aa0239b7cdf8c6e1b09f1a931469e572dacf2757
Ishika-45 <bansalishika58@gmail.com>
2024-07-16 22:40:13 +0530

    add new files

--- To-Do List/todo_app/src/redux/actions.js
+++ To-Do List/todo_app/src/redux/actions.js
@@ -1,4 +1,4 @@
-import { ADD_TODO, TOGGLE_TODO } from "./actionTypes";
+import { ADD_TODO, FILTER_TODOS, MARK_ALL_COMPLETED, MARK_COMPLETED, MARK_INCOMPLETE, REMOVE_TODO, TOGGLE_TODO, UPDATA_SEARCH_TERM } from "./actionTypes";
 
 export const addTodo = (text) => ({
     type: ADD_TODO,
@@ -10,4 +10,31 @@ export const toggleTodo = (id) => ({
     payload: {id}
 })
 
-export
+export const removeTodo = (id) => ({
+    type: REMOVE_TODO,
+    payload: {id}
+})
+
+export const markCompleted = (id) => ({
+    type: MARK_COMPLETED,
+    payload: {id}
+})
+
+export const markIncomplete =  (id) => ({
+    type: MARK_INCOMPLETE,
+    payload: {id}
+})
+
+export const filterTodos = (filter) => ({
+    type: FILTER_TODOS,
+    payload: {filter}
+})
+
+export const markAllCompleted = () => ({
+    type: MARK_ALL_COMPLETED,
+})
+
+export const updateSearchterm = (searchTerm) => ({
+    type: UPDATA_SEARCH_TERM,
+    payload: {searchTerm}
+})
