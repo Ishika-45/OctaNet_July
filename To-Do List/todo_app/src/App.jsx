4dc480bd847be97b3ff19f34a3df778a3745ea6c
Ishika-45 <bansalishika58@gmail.com>
2024-07-17 22:47:15 +0530

    add new files

--- To-Do List/todo_app/src/App.jsx
+++ To-Do List/todo_app/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
+import {Provider} from "redux"
+import store from './redux/store'
 
 function App() {
 
   return (
-    <>
+    <Provider store = {store}>
       <h1 className='text-5xl font-bold'>Hello, Developer!!</h1>
-    </>
+    </Provider>
   )
 }
 
